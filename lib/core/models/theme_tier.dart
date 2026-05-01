41a3cb9d3937364bc8290a00e29dec3e75c212c1
Tarık <tariksharabaty@gmail.com>
2026-05-02 00:35:54 +0300

    Delete lib directory

--- lib/core/models/theme_tier.dart
+++
@@ -1,9 +0,0 @@
-/// [Tema Seviyeleri | Theme Tiers]
-/// Uygulama içi ödeme sistemine bağlı tema seviyelerini tanımlar.
-/// Defines the theme tiers linked to the in-app payment system.
-enum ThemeTier {
-  defaultTier, // [Varsayılan | Default]
-  free,        // [Ücretsiz | Free]
-  premium,     // [Premium | Premium]
-  elite        // [Seçkin | Elite]
-}
--- lib/core/utils/calligraphy_paths.dart
+++
@@ -1,7 +0,0 @@
-class CalligraphyPaths {
-  // [Bismillah Vector Path - Placeholder]
-  static const String bismillahPath = "M10 10 H 90 V 90 H 10 Z";
-  
-  // Gelecekte gerçek Arapça vektörlerin (Path) matematiksel kodlarını buraya gömeceğiz.
-  // [Real Arabic vector mathematical codes will be embedded here in the future.]
-}
